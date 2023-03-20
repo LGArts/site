e462eec1fae87610132fb0fa40183d9ed6cb3b1d
LGArts <lindagreenarts@gmail.com>
2023-03-19 18:52:36 -0700

    test

--- res/script.js
+++ res/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	// change link class when on current page
 	$('a[href="' + location.pathname + '"]').addClass('current-link');
+	$("#headerhtml").load("res/header.html");
+	$("#footerhtml").load("res/footer.html");
 });
 
 $(function() {
