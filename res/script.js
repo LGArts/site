2b600a7fa67ea568d151160d99b8f8b571263f0f
LGArts <lindagreenarts@gmail.com>
2023-03-19 19:41:05 -0700

    added content

--- res/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	// change link class when on current page
-	$('a[href="' + location.pathname + '"]').addClass('current-link');
-	// inject header and footer
-	$("#headerhtml").load("res/header.html");
-	$("#footerhtml").load("res/footer.html");
-});
